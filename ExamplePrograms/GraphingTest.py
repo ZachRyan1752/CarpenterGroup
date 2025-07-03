22a945239a1f32d6a104e770789587f74c77f27e
ZachRyan1752 <ryan.1752@buckeyemail.osu.edu>
2025-07-03 13:46:49 -0400

    Add files via upload

---
+++ ExamplePrograms/GraphingTest.py
@@ -0,0 +1,21 @@
+import GeneralLibrary as GL
+import Graphing as Gph
+import tifffile
+
+DataPaths = []
+DataPaths.append(GL.AskForFiles()[0])
+DataPaths.append(GL.AskForFiles()[0])
+DataPaths.append(GL.AskForFiles()[0])
+
+DataArray = []
+LabelArray = []
+for Files in DataPaths:
+    Data = tifffile.imread(Files).ravel()
+    DataArray.append(Data)
+    LabelArray.append(Files)
+
+Bins = 1000
+HistogramFormat = "step"
+Gph.Init()
+Gph.ACSFormatSingleColumn()
+Gph.Histogram(DataArray, LabelArray, Bins, HistogramFormat, AxisLabels = ["Readout Noise (Electrons)","Count (Pixels)"], AxisScales = ["linear","log"], AxisLimits = [(0,5),(0,1e5)])
